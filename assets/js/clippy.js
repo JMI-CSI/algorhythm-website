c60965071f4a38f7582be989d34a14aac7deaba1
Ajitesh Rai <ajiteshr7@gmail.com>
2017-02-22 00:13:36 +0530

    initial

---
+++ assets/js/clippy.js
@@ -0,0 +1,30 @@
+/*---------------------------------------------
+Clippy Headings
+- Creates a scrolling cliprect effect on fixed headings
+----------------------------------------------*/
+
+(function($) {
+  var s,
+  clippy = {
+    settings: {
+      heading: $('.js-clippy'),
+    },
+    init: function() {
+      s = this.settings;
+      this.bindEvents();
+    },
+    bindEvents: function(){
+      $(window).on("load resize scroll", $.proxy(this.getClippy, this));
+    },
+
+    getClippy: function(){
+        s.heading.each(function() {
+          var layerOffset = $(this).closest('article, section').offset(),
+              containerOffset = layerOffset.top - $(window).scrollTop(),
+              clippy = containerOffset - $(this).css("top").replace(/[^-\d\.]/g, '') - $(this).css("margin-top").replace(/[^-\d\.]/g, '');
+          $(this).css('clip', 'rect('+ clippy +'px, auto, auto, auto)');
+        });
+    },
+  };
+   clippy.init();
+})(jQuery);
